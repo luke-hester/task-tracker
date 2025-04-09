f07d0589953584ae590e26414df670bf401928a2
Luke Hester <lukehester02@gmail.com>
2025-04-09 18:04:43 +0100

    Add ID to tasks

--- src/tasktracker/Task.java
+++ src/tasktracker/Task.java
@@ -1,9 +1,11 @@
 package tasktracker;
 
 public class Task {
+    int id;
     String name;
 
-    Task(String name) {
+    Task(String name, int id) {
         this.name = name;
+        this.id = id;
     }
 }
--- src/tasktracker/Tracker.java
+++ src/tasktracker/Tracker.java
@@ -3,11 +3,15 @@ package tasktracker;
 import java.util.ArrayList;
 
 public class Tracker {
+    int current_id = 0;
+
     ArrayList<Task> tasks = new ArrayList<>();
 
     void add(String name){
-        Task task = new Task(name);
+        current_id += 1;
+        int id = current_id;
+        Task task = new Task(name, id);
         tasks.add(task);
-        System.out.println("Added task " + name);
+        System.out.println("Task added successfully (ID: " + id + ")");
     }
 }
